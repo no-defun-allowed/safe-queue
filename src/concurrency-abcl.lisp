ba082c0f9b71865aa8ced922046a087af300023d
theemacsshibe <theemacsshibe@gmail.com>
2020-06-16 20:45:24 +1000

    Make the mailbox a lot faster

--- src/concurrency-abcl.lisp
+++ src/concurrency-abcl.lisp
@@ -1,9 +1,36 @@
 (in-package #:safe-queue)
 
+(declaim (optimize (speed 3)))
 (deftype mailbox ()
   '(satisfies mailbox-p))
 
+(defmacro method (class name &rest arguments)
+  `(load-time-value (java:jmethod ,class ,name ,@arguments)))
+
+(defun mailbox-p (mailbox)
+  (java:jinstance-of-p mailbox "java.util.concurrent.BlockingQueue"))
+
+(defun mailbox-send-message (mailbox message)
+  (java:jcall (method "java.util.concurrent.LinkedBlockingQueue" "put" "java.lang.Object")
+              mailbox message))
+
+(defun mailbox-receive-message (mailbox &key timeout)
+  (if (null timeout)
+      (values (java:jcall (method "java.util.concurrent.LinkedBlockingQueue" "take")
+                          mailbox)
+              t)
+      (let ((value (java:jcall-raw (method "java.util.concurrent.LinkedBlockingQueue" "poll"
+                                           "long" "java.util.concurrent.TimeUnit")
+                                   mailbox
+                                   (floor timeout 0.001)
+                                   (load-time-value #"TimeUnit.MILLISECONDS"))))
+        (if (and (java:java-object-p value)
+                 (java:jnull-ref-p value))
+            (values nil nil)
+            (values value t)))))
+
 (defun make-mailbox (&key name initial-contents)
+  (declare (ignore name))
   (let ((mailbox
           (java:jnew "java.util.concurrent.LinkedBlockingQueue")))
     (map 'nil
@@ -11,22 +38,3 @@
            (mailbox-send-message item mailbox))
          initial-contents)
     mailbox))
-
-(defvar *put* (java:jmethod "java.util.concurrent.LinkedBlockingQueue" "put" "java.lang.Object"))
-(defun mailbox-send-message (mailbox message)
-  (java:jcall *put* mailbox message))
-
-(defvar *milliseconds* #"TimeUnit.MILLISECONDS")
-(defvar *take* (java:jmethod "java.util.concurrent.LinkedBlockingQueue" "take"))
-(defvar *poll* (java:jmethod "java.util.concurrent.LinkedBlockingQueue" "poll"
-                             "long" "java.util.concurrent.TimeUnit"))
-(defun mailbox-receive-message (mailbox &key timeout)
-  (if (null timeout)
-      (values (java:jcall *take* mailbox) t)
-      (let ((value (java:jcall-raw *poll* mailbox
-                                   (floor timeout 0.001)
-                                   *milliseconds*)))
-        (if (and (java:java-object-p value)
-                 (java:jnull-ref-p value))
-            (values nil nil)
-            (values value t)))))
